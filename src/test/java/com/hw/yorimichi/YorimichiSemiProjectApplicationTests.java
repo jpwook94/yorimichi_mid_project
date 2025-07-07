009ea99e535c08a0e3bdb8d88d692289c117618d
Jung Hyunwook <jpwook94@gmail.com>
2025-07-07 17:53:12 +0900

    first commit

---
+++ src/test/java/com/hw/yorimichi/YorimichiSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.yorimichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorimichiSemiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
