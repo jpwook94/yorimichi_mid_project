d2289d9eddf380bf5b763bfdc5dfb20738678bc3
Ghaeun Lee <sallyoo1212@gmail.com>
2025-07-08 20:14:31 +0900

    전체조회 테스트 한 거

--- src/main/java/com/yorimichi/travel/controller/charTest/CharTestController.java
+++ src/main/java/com/yorimichi/travel/controller/charTest/CharTestController.java
@@ -1,4 +1,23 @@
 package com.yorimichi.travel.controller.charTest;
 
+import com.yorimichi.travel.service.charTest.CharTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CharTestController {
+
+
+    @Autowired
+    private CharTestService charTestService;
+
+    // 전체조회 테스트
+    @GetMapping("/destination-all")
+    public String destinationAll(Model model) {
+        model.addAttribute(CharTestService.getAllDestination() );
+        return "charTest/select_test";
+    }
+
 }
